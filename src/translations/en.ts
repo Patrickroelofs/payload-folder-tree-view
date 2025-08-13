357caa5805f23e66203bd3b2df78ed8165291fc7
Patrick Roelofs <contact@patrickroelofs.com>
2025-08-13 21:49:28 +0200

    feat: add translation files (#25)

---
+++ src/translations/en.ts
@@ -0,0 +1,7 @@
+import type { GenericTranslationsObject } from '@payloadcms/translations'
+
+export const en: GenericTranslationsObject = {
+  $schema: './translation-schema.json',
+  'plugin-ftv': {
+  },
+}
